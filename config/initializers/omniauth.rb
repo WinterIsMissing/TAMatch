bbc647c9302ed374c9217ff1ae6e222cdeae5a5e
EC2 Default User <ec2-user@ip-172-31-41-36.ap-southeast-1.compute.internal>
2019-01-07 02:15:41 +0000

    Oauth test

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,2 @@
+provider :google_oauth2, "488709834553-mn0prb7hjumj2p4g6emf42jcmnsqrgo6.apps.googleusercontent.com", "pna3sAqDgFf3gg6Sis8gmLEB",
+  scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google'
