42232c02bed9055c30fb927369736a04d2527827
EC2 Default User <ec2-user@ip-172-31-41-36.ap-southeast-1.compute.internal>
2019-01-07 17:46:21 +0000

    enabled both login methods

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :session, only: [:new, :create]
   root 'static#home'
-  get '/auth/:provider/callback', to: 'session#create'
+  get '/auth/:provider/callback', to: 'session#create_oauth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create]
   get '/register', to: 'users#register'
