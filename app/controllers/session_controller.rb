a643a7a550b604af0ec7e5d9f7c9fdb352f107c2
EC2 Default User <ec2-user@ip-172-31-41-36.ap-southeast-1.compute.internal>
2019-01-07 03:24:36 +0000

    Fixed flash messages

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -19,10 +19,10 @@ class SessionController < ApplicationController
   begin
     @user = User.from_omniauth(request.env['omniauth.auth'])
     session[:user_id] = @user.id
-    flash[:success] = "Welcome, #{@user.name}!"
+    flash.now[:success] = "Welcome, #{@user.name}!"
    rescue
-    flash[:warning] = "There was an error while trying to authenticate you..."
+    flash.now[:warning] = "There was an error while trying to authenticate you..."
   end
-    redirect_to root_path, notice: 'Welcome, #{@user.name}!'
+    redirect_to root_path, notice: 'Login via Google successful'
   end
 end
