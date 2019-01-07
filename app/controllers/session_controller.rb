fe5c481cfe4594a7f670b2ada917a5a5f06534b2
EC2 Default User <ec2-user@ip-172-31-41-36.ap-southeast-1.compute.internal>
2019-01-07 03:18:01 +0000

    should work now

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -14,17 +14,15 @@ class SessionController < ApplicationController
     end
   end
 
-  def new
-  end
 
   def create
   begin
     @user = User.from_omniauth(request.env['omniauth.auth'])
     session[:user_id] = @user.id
     flash[:success] = "Welcome, #{@user.name}!"
-  rescue
+   rescue
     flash[:warning] = "There was an error while trying to authenticate you..."
-  end
-  redirect_to root_path
+    end
+    redirect_to root_path
   end
 end
