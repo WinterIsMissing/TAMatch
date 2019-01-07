af12407cd8e6cf3d56b368da2130b2c34e36d975
EC2 Default User <ec2-user@ip-172-31-41-36.ap-southeast-1.compute.internal>
2019-01-07 03:02:58 +0000

    fixed application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :authenticate_user!
   def sign_in_user(user)
     user.expire_token!
     session[:email] = user.email
