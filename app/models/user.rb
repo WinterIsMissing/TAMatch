6b3c17d2f45973f4f4c2f0f868527c51f94d92ea
EC2 Default User <ec2-user@ip-172-31-41-36.ap-southeast-1.compute.internal>
2019-01-07 02:40:33 +0000

    omniauth attempt

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,4 +43,16 @@ class User < ApplicationRecord
   def token_validity
     24.hour
   end
+  
+end
+class << self
+  def from_omniauth(auth_hash)
+    user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+    user.name = auth_hash['info']['name']
+    user.location = auth_hash['info']['location']
+    user.image_url = auth_hash['info']['image']
+    user.url = auth_hash['info']['urls'][user.provider.capitalize]
+    user.save!
+    user
+  end
 end
