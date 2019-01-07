f57d22d200753ee44a8721e8c072bc9a40626d13
EC2 Default User <ec2-user@ip-172-31-41-36.ap-southeast-1.compute.internal>
2019-01-07 02:59:43 +0000

    fixed more links

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, "488709834553-mn0prb7hjumj2p4g6emf42jcmnsqrgo6.apps.googleusercontent.com", "pna3sAqDgFf3gg6Sis8gmLEB",
-  scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google_oauth2'
+  scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google'
 end
