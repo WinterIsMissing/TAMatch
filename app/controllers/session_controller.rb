bf73d42e1e7caf2f8cf6fbe2e231517791e75359
EC2 Default User <ec2-user@ip-172-31-41-36.ap-southeast-1.compute.internal>
2019-01-08 20:25:10 +0000

    add from omniauth method

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -47,8 +47,9 @@ class SessionController < ApplicationController
      
     #  @user.generate_login_token
      # @user.expire_token!
-      
+      puts 'checkpoint 1'
       session[:user_token] = @user.email
+      puts 'checkpoint 2'
       puts @user.email
       flash.now[:success] = "Welcome, #{@user.email}!"
     rescue
