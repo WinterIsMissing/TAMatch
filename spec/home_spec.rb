2dabc072a853a095ebdf22dd45383d0ed85a5fb2
EC2 Default User <ec2-user@ip-172-31-41-36.ap-southeast-1.compute.internal>
2019-01-08 15:07:20 +0000

    RSpec test cases for homepage

--- spec/home_spec.rb
+++ spec/home_spec.rb
@@ -3,10 +3,17 @@ require 'spec_helper'
 require 'capybara/rspec'
 
 
-RSpec.describe "GET TAnder homepage", :type => :feature do
+RSpec.describe "TAnder homepage", :type => :feature do
   it 'welcomes the user to our homepage' do
     visit('https://young-lowlands-69353.herokuapp.com/')
     expect(page.title).to have_content("Welcome to TAnder!")
-    
+  end
+  it 'contains Google Oauth login' do
+    visit('https://young-lowlands-69353.herokuapp.com/')
+    page.should have_content("Sign in via Google")
+  end
+  it 'contains register link for new users' do
+    visit('https://young-lowlands-69353.herokuapp.com/')
+    page.should have_content("Register!")
   end
 end
