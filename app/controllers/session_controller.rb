6a94607af259679bb3c1454351ba7f108a9b2bb6
EC2 Default User <ec2-user@ip-172-31-41-36.ap-southeast-1.compute.internal>
2019-01-08 00:57:22 +0000

    fixed session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -13,9 +13,20 @@ class SessionController < ApplicationController
       redirect_to root_path, notice: 'You have been signed in!'
     end
   end
-
-
   def create
+    puts params
+    value = params[:value].to_s
+    user = User.find_user_by(value)
+
+    if !user
+      redirect_to new_session_path, notice: "Uh oh! We couldn't find the username / email. Please try again."
+    else
+      user.send_login_link
+      redirect_to root_path, notice: 'We have sent you the link to login to our app'
+    end
+  end
+
+  def create_oauth
   begin
     @user = User.from_omniauth(request.env['omniauth.auth'])
     session[:user_id] = @user.id
