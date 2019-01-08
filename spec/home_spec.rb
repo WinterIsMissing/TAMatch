82a08f7bda813d171ddff490836e63c56fc18a16
EC2 Default User <ec2-user@ip-172-31-41-36.ap-southeast-1.compute.internal>
2019-01-08 14:34:59 +0000

    updates from Github

--- spec/home_spec.rb
+++ spec/home_spec.rb
@@ -1,4 +1,4 @@
-
+require 'rails_helper'
 require 'spec_helper'
 require 'capybara/rspec'
 
@@ -6,7 +6,7 @@ require 'capybara/rspec'
 describe "GET TAMAtch homepage", :type => :feature do
   it 'welcomes the user to our homepage' do
     visit('https://young-lowlands-69353.herokuapp.com/')
-    expect(page.title).to have_content("Welcome")
+    expect(page.title).to have_content("Welcome to TAnder!")
     puts " In the homepage "
   end
 end
