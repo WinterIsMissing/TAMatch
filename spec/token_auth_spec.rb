25ae2bf75b43f4c66626192bdfc9268bc442a700
EC2 Default User <ec2-user@ip-172-31-41-36.ap-southeast-1.compute.internal>
2019-01-09 01:46:54 +0000

    Add brute force test

--- spec/token_auth_spec.rb
+++ spec/token_auth_spec.rb
@@ -8,9 +8,14 @@ RSpec.describe "Email token auth layer", :type => :feature do
     visit('https://young-lowlands-69353.herokuapp.com/')
     expect(page.title).to have_content("Welcome to TAnder!")
   end
-  it 'fuzz test for login token' do
-    fuzz = SecureRandom.urlsafe_base64(20)
-    visit("https://young-lowlands-69353.herokuapp.com/auth?#{fuzz}")
-    expect(page).to have_content("It seems your link is invalid. Try requesting for a new login link")
+  it 'brute force for login token (100 attempts)' do
+    fuzz_list =[]
+    (1...100).each do
+      fuzz_list << SecureRandom.urlsafe_base64(20)
+    end
+    fuzz_list.each do
+      visit("https://young-lowlands-69353.herokuapp.com/auth?#{fuzz}")
+      expect(page).to have_content("It seems your link is invalid. Try requesting for a new login link")
+    end  
   end
 end
