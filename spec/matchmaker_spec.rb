d3b3690e521b38e8eb7b453445621e440ecec34e
Ryan <ryan.garmeson@gmail.com>
2019-01-09 14:14:52 +0000

    updated matchmaker rspec for instability test

--- spec/matchmaker_spec.rb
+++ spec/matchmaker_spec.rb
@@ -3,8 +3,53 @@ require "matchmaker"
 
 # spec/matchmaker.rb
 RSpec.describe Matchmaker do
+  
+    describe ".stability" do 
+      context "given unstable couples" do
+        it "returns an appropriate percentage" do
+          class Matchmaker
+            def bad_match 
+              women = @women.values
+              men = @men.values
+              matches = Hash.new
+              men.each_with_index {|k,i|matches[k] = women[i]}
+              matches.each {|m,w| m.engage(w)}
+              return matches
+            end
+          end
+          matcher = Matchmaker.new(
+                          {
+            'abe'  => %w[abi eve cath ivy jan dee fay bea hope gay],
+            'bob'  => %w[cath hope abi dee eve fay bea jan ivy gay],
+            'col'  => %w[hope eve abi dee bea fay ivy gay cath jan],
+            'dan'  => %w[ivy fay dee gay hope eve jan bea cath abi],
+            'ed'   => %w[jan dee bea cath fay eve abi ivy hope gay],
+            'fred' => %w[bea abi dee gay eve ivy cath jan hope fay],
+            'gav'  => %w[gay eve ivy bea cath abi dee hope jan fay],
+            'hal'  => %w[abi eve hope fay ivy cath jan bea gay dee],
+            'ian'  => %w[hope cath dee gay bea abi fay ivy jan eve],
+            'jon'  => %w[abi fay jan gay eve bea dee cath ivy hope],
+                          },
+                          {
+            'abi'  => %w[bob fred jon gav ian abe dan ed col hal],
+            'bea'  => %w[bob abe col fred gav dan ian ed jon hal],
+            'cath' => %w[fred bob ed gav hal col ian abe dan jon],
+            'dee'  => %w[fred jon col abe ian hal gav dan bob ed],
+            'eve'  => %w[jon hal fred dan abe gav col ed ian bob],
+            'fay'  => %w[bob abe ed ian jon dan fred gav col hal],
+            'gay'  => %w[jon gav hal fred bob abe col ed dan ian],
+            'hope' => %w[gav jon bob abe ian dan hal ed col fred],
+            'ivy'  => %w[ian col hal gav fred bob abe ed jon dan],
+            'jan'  => %w[ed hal gav abe bob jon col ian fred dan],
+          })
+          matcher.bad_match
+          expect matcher.stability < 1.0
+        end
+      end
+    end
+                
     
-    describe ".match" do
+    describe ".match_couples" do
         
         context "given empty arg" do
             it "returns nil" do
