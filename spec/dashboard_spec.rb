1df2608056820ab50d1eec648c5477af5af9f26d
EC2 Default User <ec2-user@ip-172-31-41-36.ap-southeast-1.compute.internal>
2019-01-09 04:12:29 +0000

    more dashboard tests

--- spec/dashboard_spec.rb
+++ spec/dashboard_spec.rb
@@ -34,6 +34,7 @@ RSpec.describe "dashboard", :type => :feature do
     expect(page).to have_content("Instructor")
     expect(page).to have_content("Add Posting")
     expect(page).to have_content("View Postings")
+    expect(page).to have_content("Rank Applications")
   end
   it 'directs to correct dashbaord page (admin)' do 
     email = "test_a@x.x"
@@ -41,7 +42,36 @@ RSpec.describe "dashboard", :type => :feature do
     token = @user.generate_login_token
     visit '/auth?' + token
     expect(page).to have_content("Admin")
+    expect(page).to have_field("search_query")
   end
+  it 'Create Application button test' do 
+    email = "test_s@x.x"
+    @user = User.find_by(email: email)
+    token = @user.generate_login_token
+    visit '/auth?' + token
+    expect(page).to have_content("Create Application")
+    find('a[href$="/ta_application/index"]').click
+    expect(current_path).to eq('/ta_application/index')
+  end
+  it 'Rank Applications button test' do 
+    email = "test_i@x.x"
+    @user = User.find_by(email: email)
+    token = @user.generate_login_token
+    visit '/auth?' + token
+    expect(page).to have_content("Rank Applications")
+    find('a[href$="/prof_ranking/index"]').click
+    expect(current_path).to eq('/prof_ranking/index')
+  end
+  it 'Admin page form functions' do
+    email = "test_a@x.x"
+    @user = User.find_by(email: email)
+    token = @user.generate_login_token
+    visit '/auth?' + token
+    fill_in 'search_query', with: 'CSCE110'
+    expect(page).to have_field("search_query", with: 'CSCE110')
+    
+  end  
+  
   
 
 end
