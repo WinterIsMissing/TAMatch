fe2ea8e972b81911bf0f8371d064fefdd24206db
Ryan <ryan.garmeson@gmail.com>
2019-01-09 03:59:55 +0000

    can import classes from csv

--- lib/class_import.rb
+++ lib/class_import.rb
@@ -1,41 +1,43 @@
-require 'csv'    
+require 'csv'  
+require 'set'
 
 class Course
-  def estimate_assistants(student_num)
-    return student_num.to_i / 60 + 1
+  def estimate_assistants(student_num, course_info)
+    # "a good measure is 60=1 TA, 90-100=1 TA + 1 grader. Seminars get nothing. Capstones get 1 TA"
+    @grader_count = 0
+    @ta_count = 0
+    if course_info =~ /capstone/i 
+      @ta_count = 1
+      return
+    end
+    return if course_info =~ /((\bseminar\b)|(\bresearch\b)|(\bdirected studies\b))/i
+    @ta_count = student_num.to_i / 60 
+    r = student_num.to_i.modulo(60)
+    @grader_count += 1 if r.between?(30,40)
+    @ta_count += 1 if r > 40  
   end
   
-  def initialize(name, professor, time, student_num)
+  def initialize(name, student_num, course_info)
     @name = name
-    @times = [time]
-    @professor = professor
-    @assistant_count = estimate_assistants(student_num)
+    @student_count = student_num
+    @course_info = course_info
+    estimate_assistants(student_num, course_info)
   end
-  
-  def add_time(time)
-    @times.push(time)
-  end
-  
+
   def to_s
-    "CSCE #{@name} with #{@professor} and #{assistant_count} assistants"
+    "CSCE #{@name}: #{@ta_count} TAs, #{@grader_count} graders, #{@student_count} students"
   end
 end
 
-csv_text = File.read('./test/classbook.csv')
-csv = CSV.parse(csv_text)
+csv_text = File.read('./test/coursebook_simple.csv')
+csv = CSV.parse(csv_text, :headers=>true)
 
 courses = []
 csv.each do |line|
-  student_num = line.fetch(10, nil)
-  time = "#{line.fetch(8, "")} #{line.fetch(9, "")}"
-  next if time.blank?
-  unless student_num.nil?
-    name = "#{line.fetch(3, "")}-#{line.fetch(4, "")}"
-    courses.push(Course.new(name, line.fetch(13,nil), time, student_num))
-  else
-    courses[-1].add_time(time)
-  end
-  puts courses[-1]
+  student_num = line["students"]
+  name = line["course"]
+  info = "#{line["title"]} #{line["info"]}"
+  courses.push(Course.new(name, student_num, info))
 end
 
-      
+courses.each {|x| puts x}
