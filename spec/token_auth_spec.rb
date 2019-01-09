c44ec97da05df17ee992f6bec0443a80c195f23f
EC2 Default User <ec2-user@ip-172-31-41-36.ap-southeast-1.compute.internal>
2019-01-09 02:12:25 +0000

    Added token strength test

--- spec/token_auth_spec.rb
+++ spec/token_auth_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "Email token auth layer", :type => :feature do
     visit('https://young-lowlands-69353.herokuapp.com/')
     expect(page.title).to have_content("Welcome to TAnder!")
   end
-  it 'brute force for login token (100 attempts)' do
+  it 'brute force attack for login token (100 attempts)' do
     fuzz_list =[]
     (1...100).each do
       fuzz_list << SecureRandom.urlsafe_base64(20)
@@ -18,4 +18,16 @@ RSpec.describe "Email token auth layer", :type => :feature do
       expect(page).to have_content("It seems your link is invalid. Try requesting for a new login link")
     end  
   end
+  it 'contains a decent token validity period' do
+    expect(token_validity).to be <= 24.hour
+    expect(token_validity).to be >= 15.minute
+  end
+  it "generates a secure enough token" do
+    test = SecureRandom.urlsafe_base64(20)
+    expect(test.length).to be > 20
+    checker = StrongPassword::StrengthChecker.new(test)
+    expect(checker.is_strong?).to be true 
+    expect(checker.calculate_entropy(use_dictionary: true)).to be > 33
+  end
+
 end
