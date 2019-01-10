5d0c1c3204b7da28ed3adb1bca680951a8157514
Benjamin Wong <ben.wong.jamin@gmail.com>
2019-01-10 03:54:32 +0000

    Added remove instructor functionality for InstructorListing view, added cucumber and rspec tests

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -15,4 +15,17 @@ class InstructorsController < ApplicationController
       redirect_to instructors_index_path, notice: "Added #{@email} as an instructor! :)"
     end
   end
+  def remove
+    @email = params[:email]
+    @user = User.find_by(email: @email)
+    if !@user
+      redirect_to instructors_index_path, notice: "Could not find the user #{@email}"
+    elsif @user.auth_level == "admin"
+      redirect_to instructors_index_path, notice: "User's role is admin..."
+    else
+      @user.auth_level = "student"
+      @user.save!
+      redirect_to instructors_index_path, notice: "Removed #{@email} as an instructor! :)"
+    end
+  end
 end
