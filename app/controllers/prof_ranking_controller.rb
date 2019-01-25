8a6c32b18ba7e3b65e2e2341a2415a59018ff00a
Ben <ben.wong.jamin@gmail.com>
2019-01-25 06:42:40 +0000

    Added rspec tests for InstructorPreference page w/ modifications to view to be test-friendly

--- app/controllers/prof_ranking_controller.rb
+++ app/controllers/prof_ranking_controller.rb
@@ -2,25 +2,22 @@ DEF_COURSE = "121"
 class ProfRankingController < ApplicationController
   def index
     @applicants = nil
-    query = params[:search]
-    query ||= session[:ip_search]
     course = params[:course]
     course ||= session[:ip_course]
-  
-    if !(params[:search].nil? || params[:course].nil?)
-      if query == "default"
-        @applicants = Applicant.where("'#{course}' = ANY(preferences)")
-        #Sort
-      else
-        @applicants = Applicant.all
-      end
+    
+    if params[:name] then session[:ip_name] = params[:name] end
+    if params[:advisor] then session[:ip_advisor] = params[:advisor] end
+    if params[:years] then session[:ip_years] = params[:years] end
+    if params[:yr_cmp] then session[:ip_yr_cmp] = params[:yr_cmp] end
+    
+    if !(params[:course].nil?)
+      @applicants = Applicant.where("'#{course}' = ANY(preferences)")
       @applicants = @applicants.sort_by { |applicant|
         applicant.preferences.index(course) or (1.0/0.0)
       }
     else
-      if query.nil? then query = "default" end
-      if course.nil? then course = DEF_COURSE end
-      redirect_to prof_ranking_index_path(:search => query, :course => course, 
+      course ||= DEF_COURSE
+      redirect_to prof_ranking_index_path(:course => course, 
         :name => session[:ip_name], :advisor => session[:ip_advisor],
         :years => session[:ip_years], :yr_cmp => session[:ip_yr_cmp])
     end
@@ -57,13 +54,13 @@ class ProfRankingController < ApplicationController
     
     @preferences = preferences
     
-    session[:ip_search] = query
     session[:ip_course] = course
   end
   
   def search
     if params[:query]
       course = params[:query][:course]
+      course ||= params[:query][:course2]
       name = params[:query][:name]
       advisor = params[:query][:advisor]
       years = params[:query][:years]
@@ -74,23 +71,15 @@ class ProfRankingController < ApplicationController
       end
       if name
         session[:ip_name] = name
-      else
-        session.delete(:ip_name)
       end
       if advisor
         session[:ip_advisor] = advisor
-      else
-        session.delete(:ip_advisor)
       end
       if years
         session[:ip_years] = years
-      else
-        session.delete(:ip_years)
       end
       if yr_cmp
         session[:ip_yr_cmp] = yr_cmp
-      else
-        session.delete(:ip_yr_cmp)
       end
     end
     redirect_to prof_ranking_index_path
--- spec/prof_ranking_spec.rb
+++ spec/prof_ranking_spec.rb
@@ -37,6 +37,10 @@ RSpec.describe "course_overview", :type => :feature do
     @user = User.find_by(email: email)
     token = @user.generate_login_token
     visit '/auth?' + token
+    if !@user.instructor_preference.nil?
+      @user.instructor_preference.destroy
+      @user.instructor_preference = nil
+    end
     expect(page).to have_content("Rank Applications")
     visit prof_ranking_index_path
     expect(page).to have_content("Instructor Preferences")
@@ -52,5 +56,41 @@ RSpec.describe "course_overview", :type => :feature do
     fill_in 'preference_email', with: student_email
     find('input[name="commit"]').click
     expect user.instructor_preference.preferences[email] == '4'
+  end
+  
+  it 'using different search filters in instructor preference page' do 
+    email = "test_i@x.x"
+    @user = User.find_by(email: email)
+    token = @user.generate_login_token
+    visit '/auth?' + token
+    expect(page).to have_content("Rank Applications")
+    visit prof_ranking_index_path(:years => 0, :yr_cmp => ">")
+    expect(page).to have_content("years >")
+    visit prof_ranking_index_path(:years => 0, :yr_cmp => "<")
+    expect(page).to have_content("years <")
+    visit prof_ranking_index_path(:years => 0, :yr_cmp => "=")
+    expect(page).to have_content("years =")
+    visit prof_ranking_index_path(:advisor => "random")
+    expect(page).to have_content("random")
+    visit prof_ranking_index_path(:advisor => "random", :years => 0, 
+      :yr_cmp => "=", :name => "Student", :course => "121")
+    expect(page).to have_content("121")
+  end
+  
+  it 'using search filters modal in instructor preference page' do 
+    email = "test_i@x.x"
+    @user = User.find_by(email: email)
+    token = @user.generate_login_token
+    visit '/auth?' + token
+    visit prof_ranking_index_path
+    fill_in 'query_name', with: 'Student'
+    fill_in 'query_advisor', with: 'none'
+    fill_in 'query_years', with: '3'
+    fill_in 'query_course2', with: '462'
+    select('<', :from => 'query_yr_cmp')
+    find('input[name="adv_search"]').click
+    expect(page).to have_content("years < 3")
+    find('input[name="adv_search"]').click
+    expect(page).not_to have_content("years < 3")
   end
 end
